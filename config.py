155be72ce7bcca1d8a754b3704107ba2fcdbaccd
Raushan <krshn55555@gmail.com>
2025-12-07 20:02:55 +0000

    Fix SQLite readonly error on AWS - use /tmp for database

--- config.py
+++ config.py
@@ -3,7 +3,13 @@ import os
 class Config:
     """Application configuration"""
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cloud-kitchen-secret-key-2024'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///cloud_kitchen.db'
+    
+    # Use /tmp for SQLite on AWS (writable location)
+    if os.environ.get('AWS_EXECUTION_ENV'):
+        SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/cloud_kitchen.db'
+    else:
+        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///cloud_kitchen.db'
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
 
